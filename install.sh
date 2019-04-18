1719510469ffb9b561d6f83644455d29915463da
Edvin Olofsson <eolofsso@cisco.com>
2019-04-18 11:08:07 +0200

    clean up som bugs

--- install.sh
+++ install.sh
@@ -12,31 +12,31 @@ git clone git://github.com/scmbreeze/scm_breeze.git ./.scm_breeze
 ./.scm_breeze/install.sh
 # SplitJoin
 git clone git://github.com/AndrewRadev/splitjoin.vim.git ./splitjoin
-cp ./splitjoin/plugin/* ./nvim/plugin/
-cp ./splitjoin/ftplugin/* ./nvim/ftplugin/
-cp ./splitjoin/autoload/* ./nvim/autoload/
-cp ./splitjoin/doc/* ./nvim/doc/
+cp -r ./splitjoin/plugin/* ./nvim/plugin/
+cp -r ./splitjoin/ftplugin/* ./nvim/ftplugin/
+cp -r ./splitjoin/autoload/* ./nvim/autoload/
+cp -r ./splitjoin/doc/* ./nvim/doc/
 #fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ./.fzf
 ./.fzf/install
 #linediff
 git clone git://github.com/AndrewRadev/linediff.vim.git ./linediff.vim.git
-cp ./linediff.vim.git/autoload/* ./nvim/autoload/
-cp ./linediff.vim.git/doc/* ./nvim/doc/
-cp ./linediff.vim.git/plugin/* ./nvim/plugin/
+cp -r ./linediff.vim.git/autoload/* ./nvim/autoload/
+cp -r ./linediff.vim.git/doc/* ./nvim/doc/
+cp -r ./linediff.vim.git/plugin/* ./nvim/plugin/
 #fonts
 git clone https://github.com/powerline/fonts.git --depth=1 ./fonts
 ./fonts/install.sh
 
 # Setup links
-ln -sf ./.bashrc ~/.bashrc 
-ln -sf ./.bash_aliases ~/.bash_aliases
-ln -sf ./bin ~/bin
-ln -sf ./.bash-git-promt ~/.bash-git-prompt
-ln -sf ./.scm_breeze ~/.scm_breeze
-ln -sf ./.gitconfig ~/.gitconfig
+ln -sf ~/dev/git/dotfiles/.bashrc ~/.bashrc 
+ln -sf ~/dev/git/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dev/git/dotfiles/bin ~/bin
+ln -sf ~/dev/git/dotfiles/.bash-git-prompt ~/.bash-git-prompt
+ln -sf ~/dev/git/dotfiles/.scm_breeze ~/.scm_breeze
+ln -sf ~/dev/git/dotfiles/.gitconfig ~/.gitconfig
 
 # Patch monokai colors
 cp ./monokai.vim ./nvim/plugged/vim-monokai/colors/
 
-source ~/.bashrc
+source ./.bashrc
