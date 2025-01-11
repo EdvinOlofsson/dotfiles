abaa7631196fa69e6148696bfddace5ba72dded6
Edvin Olofsson <eolofsso@cisco.com>
2025-01-11 11:35:42 +0100

    wsl cleanup 3

--- install.sh
+++ install.sh
@@ -15,20 +15,16 @@ sudo apt install -y bat
 sudo apt install -y tig
 sudo apt install -y jq
 
-
-# Install dependencies for cisco vpn
-# pip install openconnect-sso
-# pip install PyQtWebEngine
-#
-
-# PyEnv
-curl https://pyenv.run | bash
 # PyEnv Python dependencies
 sudo apt-get install zlib1g-dev libffi-dev libssl-dev \
     libbz2-dev libncurses-dev libreadline-dev libsqlite3-dev \
-    liblzma-dev python-tk python3-tk tk-dev python3-pip
+    liblzma-dev python-tk python3-tk tk-dev
 
+# PyEnv $ pip
+curl https://pyenv.run | bash
 pyenv install 3.12
+sudo apt-get install python3-pip
+pip3 install six atlassian-python-api rich jenkins python-jenkins
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
@@ -49,25 +45,23 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-python3 -m pip install --user --upgrade pynvim
-pip install six atlassian-python-api rich jenkins python-jenkins
-
 # Bash Git Prompt
 git clone https://github.com/magicmonty/bash-git-prompt.git ./.bash-git-prompt --depth=1
+
 # SCM_Breeze
 git clone git://github.com/scmbreeze/scm_breeze.git ./.scm_breeze
 ./.scm_breeze/install.sh
 
 # nvim confibg
-mkdir -p ~/.config/nvim
-ln -sf ~/dev/git/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dev/git/dotfiles/nvim/treesitter.lua ~/.config/nvim/treesitter.lua
-ln -sf ~/dev/git/dotfiles/nvim/plugin ~/.config/nvim/plugin
-ln -sf ~/dev/git/dotfiles/nvim/ftplugin ~/.config/nvim/ftplugin
-ln -sf ~/dev/git/dotfiles/nvim/doc ~/.config/nvim/doc
-ln -sf ~/dev/git/dotfiles/nvim/autoload ~/.config/nvim/autoload
-ln -sf ~/dev/git/dotfiles/nvim/UltiSnips ~/.config/nvim/UltiSnips
-ln -sf ~/dev/git/dotfiles/nvim/lua ~/.config/nvim/lua
+# mkdir -p ~/.config/nvim
+# ln -sf ~/dev/git/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+# ln -sf ~/dev/git/dotfiles/nvim/treesitter.lua ~/.config/nvim/treesitter.lua
+# ln -sf ~/dev/git/dotfiles/nvim/plugin ~/.config/nvim/plugin
+# ln -sf ~/dev/git/dotfiles/nvim/ftplugin ~/.config/nvim/ftplugin
+# ln -sf ~/dev/git/dotfiles/nvim/doc ~/.config/nvim/doc
+# ln -sf ~/dev/git/dotfiles/nvim/autoload ~/.config/nvim/autoload
+# ln -sf ~/dev/git/dotfiles/nvim/UltiSnips ~/.config/nvim/UltiSnips
+# ln -sf ~/dev/git/dotfiles/nvim/lua ~/.config/nvim/lua
 
 ln -sf ~/dev/git/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dev/git/dotfiles/.tmux ~/.tmux
@@ -78,14 +72,17 @@ cp -r ./splitjoin/plugin/* ./nvim/plugin/
 cp -r ./splitjoin/ftplugin/* ./nvim/ftplugin/
 cp -r ./splitjoin/autoload/* ./nvim/autoload/
 cp -r ./splitjoin/doc/* ./nvim/doc/
+
 #fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ./.fzf
 ./.fzf/install
+
 #linediff
 git clone https://github.com/AndrewRadev/linediff.vim.git ./linediff.vim.git
 cp -r ./linediff.vim.git/autoload/* ./nvim/autoload/
 cp -r ./linediff.vim.git/doc/* ./nvim/doc/
 cp -r ./linediff.vim.git/plugin/* ./nvim/plugin/
+
 #fonts
 git clone https://github.com/powerline/fonts.git --depth=1 ./fonts
 ./fonts/install.sh
